6362131e2c94275d401ff04cdff517a382f93951
Adam Scanga <sandmanscanga@gmail.com>
2020-05-23 18:52:33 -0400

    Updated the Flask application views that perform the vulnerable search functionality.

--- vulnerable/app/app.py
+++ vulnerable/app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from urllib.parse import unquote_plus
 from database.db_prep import db_prep
 from database.db_search import db_search
 
@@ -12,6 +13,39 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/search-get", methods=["GET"])
+def search_get():
+    if request.method == "GET" and request.query_string:
+        raw_query = request.query_string.decode("utf-8")
+        query_dict = {}
+        for fields in raw_query.split("&"):
+            field = fields.split("=")
+            query_dict[field[0]] = field[1] if len(field) <= 2 else "=".join(field[1:])
+        if "last_name" in query_dict:
+            _query = query_dict.get("last_name")
+            query = unquote_plus(_query)
+            if query:
+                result = search_database(query)
+                return render_template("search_get.html", result=result)
+    return render_template("search_get.html")
+
+
+@app.route("/search-get-blind", methods=["GET"])
+def search_get_blind():
+    if request.method == "GET" and request.query_string:
+        raw_query = request.query_string.decode("utf-8")
+        query_dict = {}
+        for fields in raw_query.split("&"):
+            field = fields.split("=")
+            query_dict[field[0]] = field[1] if len(field) <= 2 else "=".join(field[1:])
+        if "id" in query_dict:
+            _query = query_dict.get("id")
+            query = unquote_plus(_query)
+            if query:
+                result = search_database(query, by_name=False)
+    return render_template("search_get_blind.html")
+
+
 @app.route("/search-post", methods=["GET", "POST"])
 def search_post():
     if request.method == "POST":
@@ -20,6 +54,15 @@ def search_post():
             result = db_search(query, by_name=False)
             return render_template("search_post.html", result=result)
     return render_template("search_post.html")
+
+
+@app.route("/search-post-blind", methods=["GET", "POST"])
+def search_post_blind():
+    if request.method == "POST":
+        query = request.form.get("last_name")
+        if query:
+            result = search_database(query)
+    return render_template("search_post_blind.html")
 
 
 if __name__ == "__main__":
