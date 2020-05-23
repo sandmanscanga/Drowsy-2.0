3f8cd90b25b1fbe578df14b8bbe5d99e4b64662a
Adam Scanga <sandmanscanga@gmail.com>
2020-05-22 22:48:14 -0400

    Updated the app.py file with the new database module and made a small change to the associated templates.

--- vulnerable/temporary/app.py
+++ vulnerable/temporary/app.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template
 import MySQLdb
+import os
+from database.db_prep import db_prep
+from database.db_creds import USER_CREDS
+
+db_prep()
 
 
-def get_rows():
-    creds = {
-        "user": "root",
-        "passwd": "topsecret",
-        "host": "db"
-    }
-    db = MySQLdb.connect(**creds)
-    c = db.cursor()
-    c.execute("select concat('It is working!')")
-    rows = c.fetchall()
-    return rows[0][0]
+def get_results():
+    try:
+        db = MySQLdb.connect(**USER_CREDS)
+        c = db.cursor()
+        c.execute("select first_name, last_name from users;")
+        results = [f"{row[0]} {row[1]}" for row in c.fetchall()]
+        db.close()
+    except MySQLdb._exceptions.OperationalError as e:
+        raise e
+    else:
+        return results
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    result = get_rows()
-    return render_template('index.html', result=result)
+    results = get_results()
+    return render_template("index.html", results=results)
 
 
 if __name__ == "__main__":
