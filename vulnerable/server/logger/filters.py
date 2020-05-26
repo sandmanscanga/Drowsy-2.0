d19416bc66fc797bef0dcfd5c3fc6450bb4e3b91
Adam Scanga <sandmanscanga@gmail.com>
2020-05-26 01:58:50 -0400

    Updated the logger source files to be free of lint.

--- vulnerable/server/logger/filters.py
+++ vulnerable/server/logger/filters.py
@@ -1,19 +1,27 @@
+"""Module for handling logging filters"""
+# pylint: disable=too-few-public-methods
 import logging
 
 
 class DebugFilter(logging.Filter):
+    """Class for debugging log filter"""
 
     def filter(self, record):
+        """Logs record numbers greater than zero"""
         return record.levelno > logging.NOTSET
 
 
 class StdoutFilter(logging.Filter):
+    """Class for standard output log filter"""
 
     def filter(self, record):
+        """Logs record numbers between INFO and WARNING"""
         return record.levelno >= logging.INFO and record.levelno < logging.WARNING
 
 
 class StderrFilter(logging.Filter):
+    """Class for standard error log filter"""
 
     def filter(self, record):
+        """Logs record numbers greater than WARNING"""
         return record.levelno >= logging.WARNING
--- vulnerable/server/logger/formatters.py
+++ vulnerable/server/logger/formatters.py
@@ -1,19 +1,23 @@
+"""Module for handling logging formats"""
 import logging
 
 
 class DebugFormatter(logging.Formatter):
+    """Class for debugging log format"""
 
     def __init__(self):
         super().__init__("%(asctime)s  |  %(levelno)-2d  |  %(name)s  |  %(message)s")
 
 
 class StdoutFormatter(logging.Formatter):
+    """Class for standard output log format"""
 
     def __init__(self):
         super().__init__("%(message)s")
 
 
 class StderrFormatter(logging.Formatter):
+    """Class for standard error log format"""
 
     def __init__(self):
         super().__init__("[%(levelname)s] %(message)s")
