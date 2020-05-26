23ce76fac7f71ca37b6cc3816dcc27d3ed8f064f
Adam Scanga <sandmanscanga@gmail.com>
2020-05-26 02:17:26 -0400

    Updated the database utilities with docstrings and some minor refactoring to conform to PEP8

--- vulnerable/server/database/utils/db_creds.py
+++ vulnerable/server/database/utils/db_creds.py
@@ -1,11 +1,13 @@
+"""Module for handling the database credentials"""
 import json
 
 
 def _load_creds(filename):
+    """Returns the credentials from file as a JSON dictionary"""
     filedirs = __file__.split("/")[:-1] + ["..", "creds"]
     filepath = "/".join(filedirs + [filename])
-    with open(filepath, "r") as f:
-        json_data = json.load(f)
+    with open(filepath, "r") as file:
+        json_data = json.load(file)
     return json_data
 
 
--- vulnerable/server/database/utils/db_data.py
+++ vulnerable/server/database/utils/db_data.py
@@ -1,15 +1,18 @@
+"""Module to handle the mock data fed to the database"""
 import random
 
 
 def _load_wordlist(filename):
+    """Loads a wordlist and returns a list of lines from the file"""
     filedirs = __file__.split("/")[:-1] + ["..", "wordlists"]
     filepath = "/".join(filedirs + [filename])
-    with open(filepath, "r") as f:
-        words = f.read().strip().split("\n")
+    with open(filepath, "r") as file:
+        words = file.read().strip().split("\n")
     return words
 
 
 def load_users(db_size):
+    """Loads a list of tuples containing users"""
     if db_size > 999 or db_size < 1:
         raise Exception("db_size must be between 0 and 1000")
 
@@ -30,8 +33,8 @@ def load_users(db_size):
     data_group = (first_names, last_names, passwords)
 
     users = []
-    for i, n in enumerate(zip(*data_group)):
-        user = (i+1, repr(n[0]), repr(n[1]), repr(n[2]))
+    for i, group in enumerate(zip(*data_group)):
+        user = (i+1, repr(group[0]), repr(group[1]), repr(group[2]))
         users.append("(%d, %s, %s, %s)" % user)
 
     return users
