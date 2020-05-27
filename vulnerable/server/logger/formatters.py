27019b7de6750a0e66e6c81ecae8a89a6f4c9e0c
Adam Scanga <sandmanscanga@gmail.com>
2020-05-27 19:21:26 -0400

    Updated the documentation for the server logger module.

--- vulnerable/server/logger/formatters.py
+++ vulnerable/server/logger/formatters.py
@@ -3,21 +3,34 @@ import logging
 
 
 class DebugFormatter(logging.Formatter):
-    """Class for debugging log format"""
+    """Class for debugging log format
+
+    The debugging format includes the time of the log, the log level,
+    the name of the logger, and the log message.
+
+    """
 
     def __init__(self):
         super().__init__("%(asctime)s  |  %(levelno)-2d  |  %(name)s  |  %(message)s")
 
 
 class StdoutFormatter(logging.Formatter):
-    """Class for standard output log format"""
+    """Class for standard output log format
+
+    The stdout format includes the log message.
+
+    """
 
     def __init__(self):
         super().__init__("%(message)s")
 
 
 class StderrFormatter(logging.Formatter):
-    """Class for standard error log format"""
+    """Class for standard error log format
+
+    The debugging format includes the log level's name, and the log message.
+
+    """
 
     def __init__(self):
         super().__init__("[%(levelname)s] %(message)s")
--- vulnerable/server/logger/handlers.py
+++ vulnerable/server/logger/handlers.py
@@ -10,7 +10,16 @@ from server.logger.filters import (
 
 
 class DebugHandler(RotatingFileHandler):
-    """Class for debugging log handler"""
+    """Class for debugging log handler
+
+    The DebugHandler will assign a rotating log file every 100,000 bytes.
+    There can be up to five rotated backups for the log path.  The
+    DebugFormatter and the DebugFilter are also added to the handler instance.
+
+    Args:
+        logpath (str): the fullpath to the logfile to rotate
+
+    """
 
     def __init__(self, logpath):
         super().__init__(logpath, maxBytes=100_000, backupCount=5)
@@ -19,7 +28,13 @@ class DebugHandler(RotatingFileHandler):
 
 
 class StdoutHandler(logging.StreamHandler):
-    """Class for standard output log handler"""
+    """Class for standard output log handler
+
+    The StdoutHandler will assign the sys.stdout descriptor and any
+    logs that are passed as info level, will be printed to the console.
+    The formatter and filter are also added to the handler configuration.
+
+    """
 
     def __init__(self):
         super().__init__(sys.stdout)
@@ -28,7 +43,14 @@ class StdoutHandler(logging.StreamHandler):
 
 
 class StderrHandler(logging.StreamHandler):
-    """Class for standard error log handler"""
+    """Class for standard error log handler
+
+    The StderrHandler will assign the sys.stderr descriptor and any
+    logs that are passed as warning level or higher, will be printed to the
+    console as an error stream. The formatter and filter are also added to
+    the handler configuration.
+
+    """
 
     def __init__(self):
         super().__init__(sys.stderr)
